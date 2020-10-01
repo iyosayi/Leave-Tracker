6d3624693263372193dab69d5dcd3d3bef68b81b
13samyakjain <samyakjain0113@gmail.com>
2020-10-01 15:41:02 +0530

    add

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './components/Login/Login';
+import addLeave from './components/Leave/Leave';
+import getLeave from './components/Leave/GetLeave/GetLeave'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Allotleave from './components/Leave/AllotLeave/Allotleave';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path='/' component={Login} />
+          <Route exact path='/addleave' component={addLeave} />
+          <Route exact path='/getleave' component={getLeave}/>
+          <Route exact path='/allotleave' component={Allotleave}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
