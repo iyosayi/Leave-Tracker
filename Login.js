030ea7798419c99a0ca296ba242a51df30574ea1
Samyak Jain <57790761+13samyakjain@users.noreply.github.com>
2020-10-01 12:35:00 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -8,7 +8,7 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import './Login.css';
+import './login.css';
 import axios from 'axios'
 import { Router,Redirect } from 'react-router-dom';
 
